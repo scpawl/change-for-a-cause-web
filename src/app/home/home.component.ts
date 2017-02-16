cbdd886621d66b34d221074e175ad4438973004d
Shannon <shannon.pawloski@capitalone.com>
2017-02-16 00:01:46 -0500

    add sample data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,67 @@
 import {Component} from '@angular/core';
 
+import { Pledge } from '../pledge/pledge';
+import { Activity } from '../activity/activity';
+
 @Component({
     selector: 'home',
     templateUrl: './app/home/home.component.html'
 })
 
 export class HomeComponent {
+  pledges: Pledge[] = [ //TODO: Replace this with mock data, then connect to full API
+    {
+      pledgeID: 1,
+      pledgeName: 'pledge 1',
+      pledgeUnits: 2,
+      pledgeStartDate: new Date("2017-02-14 04:00"),
+      pledgeEndDate: new Date("2017-02-28 04:00"),
+      pledgeMemberActivityId: 1,
+      pledgeCauseId: 1,
+      pledgeComments: 'Something about this pledge',
+      activity: null,
+      active: true,
+      causeName: 'FeedMore',
+      completedUnits: 1,
+      earnedValue: 10,
+      checkInsForPledge: null
+    },
+    {
+      pledgeID: 2,
+      pledgeName: 'pledge 2',
+      pledgeUnits: 5,
+      pledgeStartDate: new Date('2017-02-14 04:00'),
+      pledgeEndDate: new Date('2017-02-28 04:00'),
+      pledgeMemberActivityId: 1,
+      pledgeCauseId: 1,
+      pledgeComments: 'Something about this pledge',
+      activity: null,
+      active: true,
+      causeName: 'FeedMore',
+      completedUnits: 1,
+      earnedValue: 10,
+      checkInsForPledge: null
+    }
+  ];
+
+  activities: Activity[] = [  //TODO: Replace this with mock data, then connect to full API
+    {
+      activityName: 'Bike to Work',
+      activityDescription: 'Ride my bike to and from work one day to save gas money',
+      activityID: 1
+    },
+    {
+      activityName: 'Pack my Lunch',
+      activityDescription: 'Pack my lunch for a day',
+      activityID: 2
+    },
+    {
+      activityName: 'Skip my Coffee',
+      activityDescription: 'Skip purchasing coffee at Starbucks and drink the free tea instead',
+      activityID: 3
+    }
+  ];
+
+  amountSavedSoFar = 5.5;
+
 }
